20220e765988d1086b18f88c741afb76e9ee2114
kumari13 <kumari13@avaya.com>
2021-09-07 23:15:51 +0530

    ESLint also added

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -2,5 +2,7 @@ const sum = require("../src/sum")
 
 it('Sum of two numbers should be correct', () => {
   const result = sum(3, 4)
-  expect(result).toBe(7)
+  expect(result).toBe(7) // for the wrong test cases it will not commit the changes
 })
+
+// npm install pre-commit and the hooks will be added to .git/hooks
